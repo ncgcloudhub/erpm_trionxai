c7b43f5c00d4a69e14124130014e5649c2e53572
ifazalam1 <ifaz.alam1@gmail.com>
2024-01-07 22:07:10 +0600

    svg

--- resources/views/admin/Backend/Supplier/supplier.blade.php
+++ resources/views/admin/Backend/Supplier/supplier.blade.php
@@ -1,126 +1,143 @@
 @extends('admin.aDashboard')
 @section('admins')
 
-	  <div class="container-full">
-		<!-- Content Header (Page header) -->
-		  
+ {{-- TRIAL START --}}
+ <div class="container-fluid">
+	<div class="row mt-4">
+	 
 
-		<!-- Main content -->
-		<section class="content">
+{{-- ADD Student --}}
+<div class="col-lg-12 mb-lg-0 mb-4">
+  <div class="card">
+    <div class="card-body p-3">
+      <div class="row">
 
-		 <!-- Basic Forms -->
-		  <div class="box">
-			<div class="box-header with-border">
-			  <h4 class="box-title">Add Supplier </h4>
-			   
-			</div>
-			<!-- /.box-header -->
-			<div class="box-body">
-			  <div class="row">
-				<div class="col">
+		<div class="col">
 
-  <form method="post" action="{{ route('supplier.store') }}">
-		 	@csrf
+<form method="post" action="{{ route('supplier.store') }}">
+@csrf
+   
+<div class="form-group">
+<label  class="text-uppercase text-dark text-xs font-weight-bold ">Vendor Name<span class="text-danger">*</span></label>
+<div class="controls">
+<input type="text"  name="course_name" class="form-control" required > 
 
-			 <div class="form-group row">
-				<label for="supplier_name" class="col text-right col-form-label">Supplier Name <i class="text-danger"> * </i>:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input type="text" name="supplier_name" class="form-control" id="supplier_name"  value="">
-				@error('supplier_name') 
-				<span class="text-danger">{{ $message }}</span>
-				@enderror 
-				</div>
-				</div>
-				<label for="supplier_mobile" class="col text-right col-form-label">Mobile No <i class="text-danger"> </i>:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input type="number" name="mobile" class="form-control" id="supplier_mobile" value="">
-				{{-- @error('mobile') 
-				<span class="text-danger">{{ $message }}</span>
-				@enderror  --}}
-				</div>
-				</div>
-				</div>
-				<div class="form-group row">
-				<label for="email_address" class="col text-right col-form-label">Email:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input type="email" class="form-control" name="email_address" id="email_address" value="">
-				{{-- @error('email_address') 
-				<span class="text-danger">{{ $message }}</span>
-				@enderror  --}}
-				</div>
-				</div>
-				<label for="zip" class="col-sm-2 text-right col-form-label">Zip code:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input name="zip" type="text" class="form-control" id="zip"  value="">
-				</div>
-				</div>
-				</div>
-				<div class="form-group row">
-				<label for="phone" class="col-sm-2 text-right col-form-label">Phone:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input class="form-control input-mask-trigger text-left" id="phone" type="number" name="phone" value="">
-				</div>
-				</div>
+</div>
+</div>
 
-				</div>
-				<div class="form-group row">
-				<label for="address1" class="col-sm-2 text-right col-form-label">Address:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<textarea name="address" id="address" class="form-control" ></textarea>
-				{{-- @error('address') 
-				<span class="text-danger">{{ $message }}</span>
-				@enderror  --}}
-				</div>
-				</div>
 
-				</div>
-				<div class="form-group row">
-				<label for="city" class="col-sm-2 text-right col-form-label">City:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input type="text" name="city" class="form-control" id="city"  value="">
-				</div>
-				</div>
-				</div>
-				<div class="form-group row">
-				<label for="state" class="col-sm-2 text-right col-form-label">State:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input type="text" name="state" class="form-control" id="state"  value="">
-				</div>
-				</div>
+<div class="form-group">
+<label  class="text-uppercase text-dark text-xs font-weight-bold ">Course Code<span class="text-danger">*</span></label>
+<div class="controls">
+<input type="text"  name="code" class="form-control" required > 
 
-				</div>
-				<div class="form-group row">
-				<label for="country" class="col-sm-2 text-right col-form-label">Country:</label>
-				<div class="col-sm-4">
-				<div class="">
-				<input name="country" type="text" class="form-control " value="" id="country">
-				</div>
-			</div>
-			</div>
-		
-				<input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add Supplier">
-						</div>
-					</form>
+</div>
+</div>
 
-				</div>
-				<!-- /.col -->
-			  </div>
-			  <!-- /.row -->
-			</div>
-			<!-- /.box-body -->
-		  </div>
-		  <!-- /.box -->
 
-		</section>
-		<!-- /.content -->
-	  </div>
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Course Description</label>
+	<div class="controls">
+		<textarea name="description" class="form-control"  id="tinymceExample" rows="10"></textarea>	
+	</div>
+</div>
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Course Type<span class="text-danger">*</span></label>
+<div class="controls">
+<input type="text"  name="type" class="form-control" required > 
+</div>
+</div>
+
+	
+<div class="form-group">
+	<label class="text-uppercase text-dark text-xs font-weight-bold">Course Fees <span class="text-danger">*</span></label>
+	<div class="controls">
+	  <input type="text" name="fees" class="form-control" required>
+	</div>
+  </div>
+  
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Course Discounted Fees </label>
+<div class="controls">
+	<input type="text"  name="discounted_fees" class="form-control" > 
+</div>
+</div>
+
+</div>
+{{-- end column --}}
+
+<div class="col">
+
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Course Difficulty <span class="text-danger">*</span></label>
+<div class="controls">
+<input type="text" name="difficulty" class="form-control" required >
+
+</div>
+</div>
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Course Duration<span class="text-danger">*</span></label>
+<div class="controls">
+<input type="text" name="duration" class="form-control" required >
+
+</div>
+</div>
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Prerequisites </label>
+<div class="controls">
+<input type="text" name="prerequisites" class="form-control"  >
+
+</div>
+</div>
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Learning Objectives/Outcomes </label>
+<div class="controls">
+<input type="text" name="objectives" class="form-control"  >
+
+</div>
+</div>
+
+<div class="form-group">
+	<label  class="text-uppercase text-dark text-xs font-weight-bold ">Additional Notes/Comments </label>
+<div class="controls">
+<input type="text" name="comments" class="form-control"  >
+
+</div>
+</div>
+
+
+
+{{-- <div class="text-xs-right"> --}}
+	<input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add Course">					 
+	{{-- </div> --}}
+
+
+</div>{{-- end column --}}
+
+
+
+       </div>
+</form>
+
+</div>
+
+</div>
+
+
+
+{{-- TRIAL END --}}
+
+    </div>
+    @include('admin.body.footer')
+  </div>
+
+  
+
 
 @endsection
