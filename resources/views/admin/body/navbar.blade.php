a946639537aa189c37d568dc0fda0020844d3092
Fahmid Home <fahmidh26@gmail.com>
2024-01-10 21:45:50 +0600

    Breadcrumbs Fahmid Home

--- resources/views/admin/body/navbar.blade.php
+++ resources/views/admin/body/navbar.blade.php
@@ -1,11 +1,18 @@
+@php
+        $fullUrl = request()->fullUrl();
+        $prefix = request()->route()->getPrefix();
+        $pathAfterPrefix = ucfirst(ltrim(Str::after($fullUrl, $prefix), '/'));
+    @endphp
+
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
 	<div class="container-fluid py-1 px-3">
 	  <nav aria-label="breadcrumb">
 		<ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-		  <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-		  <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
+		  <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">{{ ucfirst($prefix) }}</a></li>
+		  <li class="breadcrumb-item text-sm text-dark active" aria-current="page">{{ $pathAfterPrefix }}</li>
 		</ol>
-		<h6 class="font-weight-bolder mb-0">Dashboard</h6>
+		<h6 class="font-weight-bolder mb-0">{{ ucfirst($prefix) }}</h6>
+
 	  </nav>
 	  <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
 		<div class="ms-md-auto pe-md-3 d-flex align-items-center">
