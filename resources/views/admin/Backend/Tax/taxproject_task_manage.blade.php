5a12d917d1e066ca46b6a5010dc69a84b3968825
ifazalam1 <ifaz.alam1@gmail.com>
2024-02-01 17:27:35 +0600

    import customer task disabled

--- resources/views/admin/Backend/Tax/taxproject_task_manage.blade.php
+++ resources/views/admin/Backend/Tax/taxproject_task_manage.blade.php
@@ -5,7 +5,7 @@
 
  {{-- TRIAL START --}}
  <div class="container-fluid">
-	<a href="{{route('import.tasks.customers')}}" class="btn bg-gradient-warning">Import</a>
+	{{-- <a href="{{route('import.tasks.customers')}}" class="btn bg-gradient-warning">Import</a> --}}
 	<a href="{{ route('export.tasks.customers') }}" class="btn bg-gradient-info">Export</a>
 	<div class="row mt-4">
 	  <div class="col-lg-12 mb-lg-0 mb-4">
